2d4b7611eb33a6111bd84ddc0fe5e37691c27369
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2024-04-08 04:34:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,24 +18,7 @@ def get_wavlm():
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-print("Loading FreeVC...")
-hps = utils.get_hparams_from_file("configs/freevc.json")
-freevc = SynthesizerTrn(
-    hps.data.filter_length // 2 + 1,
-    hps.train.segment_size // hps.data.hop_length,
-    **hps.model).to(device)
-_ = freevc.eval()
-_ = utils.load_checkpoint("checkpoints/freevc.pth", freevc, None)
 smodel = SpeakerEncoder('speaker_encoder/ckpt/pretrained_bak_5805000.pt')
-
-print("Loading FreeVC(24k)...")
-hps = utils.get_hparams_from_file("configs/freevc-24.json")
-freevc_24 = SynthesizerTrn(
-    hps.data.filter_length // 2 + 1,
-    hps.train.segment_size // hps.data.hop_length,
-    **hps.model).to(device)
-_ = freevc_24.eval()
-_ = utils.load_checkpoint("checkpoints/freevc-24.pth", freevc_24, None)
 
 print("Loading FreeVC-s...")
 hps = utils.get_hparams_from_file("configs/freevc-s.json")
@@ -56,6 +39,8 @@ import ffmpeg
 import urllib.request
 urllib.request.urlretrieve("https://download.openxlab.org.cn/models/Kevin676/rvc-models/weight/UVR-HP2.pth", "uvr5/uvr_model/UVR-HP2.pth")
 urllib.request.urlretrieve("https://download.openxlab.org.cn/models/Kevin676/rvc-models/weight/UVR-HP5.pth", "uvr5/uvr_model/UVR-HP5.pth")
+urllib.request.urlretrieve("https://download.openxlab.org.cn/models/Kevin676/rvc-models/weight/freevc-24.pth", "checkpoints/freevc-24.pth")
+urllib.request.urlretrieve("https://download.openxlab.org.cn/models/Kevin676/rvc-models/weight/pretrained_bak_5805000.pt", "speaker_encoder/ckpt/pretrained_bak_5805000.pt")
 
 from uvr5.vr import AudioPre
 weight_uvr5_root = "uvr5/uvr_model"
